bfbb25b70f081314e358760321e14de31a05c819
Zach Beaupre <32720538+ZachBeaupre@users.noreply.github.com>
2025-11-25 12:16:57 -0500

    Delete src/gui.java (I FIGURED IT OUT YAY)
    BE GONE

--- src/gui.java
+++
@@ -1,6 +0,0 @@
-///delete me
-///old version of the program that i didnt want to upload to github but accidentally did and now i am really really sad and i might even cry im so sad
-
-
-///all unfunny jokes aside this file is not needed in order for the program to operate so i removed it
-///i dont know how to get rid of it entirely so once I figure it out this will be gone
